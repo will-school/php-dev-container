aa30dec6c4ebacee8f420bbf2c17e929a5ccbe58
gw14dempster <gw14dempsterwilliam@glow.sch.uk>
2025-06-05 13:31:21 +0000

    Add auto link and auto start functionality, as well as a placeholder index.php page to verify it works

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index.php</title>
+</head>
+<body>
+    <h1>/index.php</h1>
+    <p>Edit index.php to get started</p>
+    <p>
+        <?php
+            echo "Hello World"
+        ?>
+    </p>
+</body>
+</html>
